d587e150bc4e797d8041273b17a21f226ad11213
KhongCL <136788485+KhongCL@users.noreply.github.com>
2023-10-10 05:32:01 +0000

    Changed CryptoCart to ShopCryptopia

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          CryptoCart
+          ShopCryptopia
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <WalletMultiButton />
@@ -34,7 +34,7 @@ function Home() {
       </div>
 
       <div className="relative flex flex-col justify-between place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <h1 className='font-bold'>CryptoCart</h1>
+        <h1 className='font-bold'>ShopCryptopia</h1>
           {/* if publicKey found, display button */}
           {publicKey ? <div className='place-items-center grid mt-10'>
           <Button onClick={handleWalletBalanceRequest}>Request Wallet Balance</Button>
